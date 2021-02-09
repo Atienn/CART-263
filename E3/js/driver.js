c24d8e30056ab4cd6338333a967bb0bf01781c1a
Atienn <Atienn@users.noreply.github.com>
2021-02-09 15:23:45 -0500

    E3: Started Exercise

---
+++ E3/js/driver.js
@@ -0,0 +1,31 @@
+/*
+  Exercise 3
+  
+*/
+
+
+/**
+ * This function is called and completed before others start.
+ */
+function preload()
+{
+
+}
+
+/**
+ * Is called once on startup.
+ */
+function setup()
+{
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+}
+
+
+/**
+ * Is called once per frame.
+ */
+function draw()
+{
+
+}
