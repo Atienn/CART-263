6c1059ab9e15486d2e3054d377f1a14cd4a5a2cc
Atienn <Atienn@users.noreply.github.com>
2021-02-09 16:36:27 -0500

    E3: Added Keyboard Input Reception

--- E3/js/driver.js
+++ E3/js/driver.js
@@ -3,6 +3,13 @@
   
 */
 
+let userProfile = 
+{
+  username: ``,
+  password: ``
+}
+
+let userInput = ``;
 
 /**
  * This function is called and completed before others start.
@@ -19,6 +26,9 @@ function setup()
 {
     createCanvas(windowWidth, windowHeight);
     background(0);
+    fill(200);
+    textSize(50);
+    textAlign(CENTER, CENTER);
 }
 
 
@@ -27,5 +37,30 @@ function setup()
  */
 function draw()
 {
+    background(0);
+    text(userInput, width/2, height/2);
+}
 
+
+/**
+ * Adds the pressed key to the user input if it is a letter.
+ * Is called when the user types a key (ignores function keys like Shift and Control).
+ */
+function keyTyped()
+{
+  //Makes the key's string uppercase to simplify verification.
+  key = key.toUpperCase();
+
+  //Checks if the pressed key contains a character between A and Z.
+  //Since function keys are ignored, only letter keys should be able to be added.
+  if(/[A-Z]/.test(key)) { userInput += key; }
+}
+
+/**
+ * Clears user input if the backspace key is pressed.
+ * Is called whenever the user is types a key on their keyboard.
+ */
+function keyPressed() 
+{
+    if (keyCode === BACKSPACE) { userInput = ``; }
 }
