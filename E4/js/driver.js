e817973f48d1af1cb934c4db0d87b7b3d135e73c
Atienn <Atienn@users.noreply.github.com>
2021-02-16 18:12:16 -0500

    E4: Adapted E3 files

--- E4/js/driver.js
+++ E4/js/driver.js
@@ -7,8 +7,6 @@
 
 "use strict";
 
-//Tracks the locally stored data.
-let userData;
 //The game's state.
 let state;
 
@@ -24,18 +22,13 @@ function setup()
     
     //Slow framerate. The game has no animation so this just 
     //lightens the computational load at basically no cost.
-    frameRate(20);
+    frameRate(30);
 
     //Make recatangles drawn from the center (applies for all game states).
     rectMode(CENTER);
 
-    //Get locally stored data if any is available.
-    userData = JSON.parse(localStorage.getItem(`E3:UserData`));
-    //If user data doesn't exist yet, create new empty data holder.
-    if(!userData) { resetUserData(); }
-
     //Start the game in the leaderboard state.
-    switchState(leaderboard);
+    switchState(menu);
 }
 
 
@@ -44,13 +37,6 @@ function setup()
  * Calls the current state's update.
  */
 function draw() { state.update(); }
-
-
-/**
- * Is called when the user types a key (ignores function keys like Shift and Control).
- * Calls the current state's key typed event.
- */
-function keyTyped() { state.keyType(); }
 
 
 /**
@@ -78,45 +64,3 @@ function switchState(newState)
     newState.setup();
 }
 
-
-/**
- * Tries to add a user to the local storage. 
- * Will fail if the new user's name is already taken.
- * @param {String} userName The new user's name.
- * @param {Number} userScore The new user's score in Quicktype.
- * @returns {boolean} If the user was successfully added to the array.
- */
-function addNewUser(userName, userScore)
-{
-    //Assume the given name isn't already taken.
-    let alreadyTaken = false;
-
-    //Verify and correct the assumption if it was wrong. 
-    for(let i = 0; i < userData.users.length; i++)
-    {
-        if(userData.users[i].name == userName) { alreadyTaken = true; }
-    }
-
-    //Don't add the user to the list if its name has already been taken.
-    if(!alreadyTaken) 
-    {
-        //Simultaneously add a new user to the users array and updates the user count.
-        userData.userCount = userData.users.push({ name: userName, score: userScore });
-
-        //Sort the array by user score (from highest to lowest).
-        userData.users.sort((userA, userB) => { return userB.score - userA.score; })
-
-        //Override the old user data to save the new user.
-        localStorage.setItem(`E3:UserData`, JSON.stringify(userData));
-    }
-
-    //Returns if the user was successfully added to the list.
-    return !alreadyTaken;
-}
-
-/** Overrides current user data (if any) with an empty user list. */
-function resetUserData()
-{
-    userData = { users: [] };
-    localStorage.setItem(`E3:UserData`, JSON.stringify(userData));
-}
--- E4/js/game.js
+++ E4/js/game.js
@@ -3,14 +3,12 @@
 /** Displays letters the user must type to increase thier score. */
 let game =
 {
-    //The letter the user must match.
-    letter: '',
-    //The last letter displayed. Used to avoid showing the same letter twice in a row.
-    lastLetter: '',
     //The game's timer.
     timer: 0,
     //The user's current score.
     score: 0,
+
+    bubbles: {},
 
     /** Sets up display settings and resets game values. */
     setup() 
@@ -25,9 +23,9 @@ let game =
 
         //Reset score, first letter and timer.
         this.score = 0;
-        this.newLetter();
         this.timer = 300;
     },
+
 
     /** Displays the letter the user must type as well as the time remaining. */
     update() 
@@ -40,13 +38,13 @@ let game =
 
         //Instructions at the top of the screen.
         textSize(50);
-        text('Type as many letters as you can!', width/2, 35);
+        text('Pop as many bubbles as you can!', width/2, 35);
         textSize(35);
         text(`Current score: ${this.score}.`, width/2, 100);
 
-        //Display the letter to type in the center.
+        //Display the [...] in the center.
         textSize(200);
-        text(this.letter, width/2, height/2);
+        circle();
 
         //Display the remaining time as a line at the bottom of the screen.
         stroke(150);
@@ -55,32 +53,18 @@ let game =
         //Ticks the timer down if it's not over.
         if(this.timer > 0) { this.timer--; }
         //Otherwise, switch to the next state where the user can enter thier name.
-        else { switchState(logUser); }
-    },
-
-    /** Checks if the typed key matches the displayed letter. */
-    keyType() 
-    {
-        //If the key matches, increase score and generate a new letter.
-        if(key.toUpperCase() === this.letter)
-        {
-            this.score++;
-            this.lastLetter = this.letter;
-            this.newLetter();
-        }
+        else { switchState(); }
     },
 
     /** Skips the game phase. Used for debug/testing. */
     keyPress() 
     {
-        if(keyCode === BACKSPACE) { this.timer = 0; }
+        if(keyCode == BACKSPACE) { switchState(menu); }
     },
 
     /** Gives a new character from A to Z to the 'letter' variable that is different from the last. */
-    newLetter()
+    newBubble()
     {
-        do { this.letter = String.fromCharCode(Math.floor(Math.random() * 26 + 65)); }
-        //If the new letter matches the old one, pick again.
-        while (this.letter === this.lastLetter);
+
     }
 }
