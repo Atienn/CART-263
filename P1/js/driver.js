dfb5cf08c7108ee3df9a199bc9d79403de806c44
Atienn <70545238+Atienn@users.noreply.github.com>
2021-03-07 12:42:33 -0500

    Implemented Voice Commands & Core Image Fetching Loop

--- P1/js/driver.js
+++ P1/js/driver.js
@@ -7,14 +7,28 @@ Built off template-p5-project.
 "use strict"
 
 let imgInfo;
+let imgTags;
 let imgHolder;
+let imgLoading;
 let infoRequest;
 
+let output;
+
+//HTML elements.
 let sideRight;
 let centerArea;
 let canvas;
+
+//
 let currentImage;
 
+let currentTimeout = "";
+
+
+function preload()
+{
+    imgTags = loadJSON("assets/data/tags.json", () => { imgTags.length = Object.keys(imgTags).length; });
+}
 
 function setup()
 {
@@ -22,29 +36,31 @@ function setup()
     if(annyang)
     {
         //Makes annyang call [] with parameter ' ' when it recognizes speech.
-        //annyang.addCommands({ '*var_name': func_name });
+        annyang.addCommands({ '*command': userCall });
         //Stars listening for voice input.
-        //annyang.start();
+        annyang.start();
     }
 
     //Get the reference of HTML elements.
     centerArea = document.getElementById("center");
     sideRight = document.getElementById("right");
+    output = document.getElementById("output");
     
 
     //Create canvas and make it fit within the center area.
     canvas = createCanvas(centerArea.clientWidth, centerArea.clientHeight);
     canvas.parent(centerArea);
 
-    //Light grey bacground.
-    background(200);
     //Draw images from the center.
     imageMode(CENTER);
     
 
     //Create an html holder for the image.
     imgHolder = document.createElement('img');
-    //imgHolder.style = "visibility: hidden;";
+    
+    //imgHolder.style = "visibility: hidden;"
+
+    //
     document.body.appendChild(imgHolder);
 
     //Loading an image after assigning it a new source is asynchronous.
@@ -56,12 +72,49 @@ function setup()
 
         //Stop rendering the html image.
         imgHolder.src = "";
+
+        //Now that the image has loaded, let other requests happen.
+        imgLoading = false;
     }
 }
 
 
+function userCall(command)
+{
+    if(command.toLowerCase().includes("ignore"))
+    {
+        outputText('Ignored.', 1250);
+        showImage(randomTag());
+    }
+    else if(command.toLowerCase().includes("delete"))
+    {
+        outputText('Deleting...', 1250);
+        showImage(randomTag());
+    }
+    else
+    {
+        outputText(`Unknown command: '${command}'`);
+    }
+}
+
+
+function randomTag() { return randomElem(randomElem(imgTags)); }
+
+function randomElem(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
+
+
 function showImage(tag)
 {
+    //End the function early if a another request is being made.
+    if(imgLoading) 
+    {
+        outputText('Image is still loading...')
+        return; 
+    }
+
+    //Prevent new requests from being made while this one is being sent/received.
+    imgLoading = true;
+
     //Create a new request.
     infoRequest = new XMLHttpRequest();
 
@@ -101,15 +154,28 @@ function showImage(tag)
 
 /**
  * Draws a new image onto the canvas while conserving its aspect ratio.
- * @param {Image} img 
+ * @param {Image} img The image to display.
  */
 function displayImage(img)
 {
-    background(200);
+    background(220);
     if( (img.width - width) > (img.height - height))
     { image(currentImage, width/2, height/2, width, img.height * (width/img.width)); }
     else
     { image(currentImage, width/2, height/2, img.width * (height/img.height), height); }
+}
+
+
+/**
+ * Displays text at the bottom of the screen for a set amount of time. 
+ * @param {String} text The string of text to display.
+ * @param {Number} duration (Optional) The amount of time (in ms) to display for.
+ */
+function outputText(text, duration = 3000)
+{
+    clearTimeout(currentTimeout);
+    output.innerHTML = text;
+    currentTimeout = setTimeout(() => { output.innerHTML = ""; }, duration)
 }
 
 
@@ -121,6 +187,6 @@ function windowResized()
 {
     resizeCanvas(centerArea.clientWidth, centerArea.clientHeight);
 
-    //Redraw what was on canvas.
+    //Redraw what was on canvas if an image was being displayed.
     if(currentImage) { displayImage(currentImage); }
 }
