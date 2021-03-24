f0dc6fe5cd8f7cce699d6c6c67ed2bc08e7906b8
Atienn <70545238+Atienn@users.noreply.github.com>
2021-03-23 23:05:29 -0400

    E7: Implemented Number Adding

--- E7/js/script.js
+++ E7/js/script.js
@@ -1,3 +1,65 @@
+/****************
+Exercise 7
+
+
+****************/
+
+"use strict";
+
+function set(num1, num2) {
+    return num1;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+
+let currentOperation = add;
+
+
 window.onload = () => {
-    
-} 
+
+
+    $(`#end-popup`).dialog({
+        autoOpen: false,
+        buttons: {
+            "Reset?": function() {
+                location.reload();
+            }
+        }
+    });
+     
+    $(`#radioset`).buttonset();
+
+    $(`.number`).draggable();
+
+    $(`#drag-box`).text(0);
+    $(`#drag-box`).droppable({
+        drop: function(event, ui) {
+            $(this).text(currentOperation(Number.parseInt($(this).text()), Number.parseInt(ui.draggable.text())));
+            ui.draggable.remove();
+        }
+    });
+}
+
+
+// Let the user drag secret letters via a clone helper
+
+
+// When the user drops a letter on the answer...
+
+
+
