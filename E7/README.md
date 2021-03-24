65cda3dabbdc49ce0ab8c50027dbae5f939ab1c2
Atienn <70545238+Atienn@users.noreply.github.com>
2021-03-24 00:37:53 -0400

    E7: Commented and Cleaned Files

--- E7/README.md
+++ E7/README.md
@@ -1,2 +1,3 @@
 # Exercise 7
 
+This program lets the user compute basic numerical operation on numbers by dragging them into a box.
--- E7/js/script.js
+++ E7/js/script.js
@@ -1,7 +1,8 @@
 /****************
 Exercise 7
 
-
+This program lets the user compute basic numerical 
+operation on numbers by dragging them into a box.
 ****************/
 
 "use strict";
@@ -31,36 +32,47 @@ function div(num1, num2) {
     return num1 / num2;
 }
 
+//The currently selected operation (default is add).
 let currOperation = set;
 
 //#endregion
 
-
+/** Only execute once the page has loaded. */
 window.onload = () => {
-
+ 
+    //Set a popup for when the user runs out of numbers.
     $(`#end-popup`).dialog({
+        //Don't open on starup.
         autoOpen: false,
+        //Single button that reloads the page.
         buttons: {
             "Reset": function() {
                 location.reload();
             }
         }
     });
-     
+    
+    //Set the buttons with thier themes.
     $(`#radioset`).buttonset();
 
+    //Listen to value changes on the radio buttons.
     $(`#radioset`).on(`change`, function() {
+        //Set the current operation to the global function matching the selected button's value.
         currOperation =  window[$("input[name='operation']:checked").val()];
-
     });
 
+    //Makes each number draggable.
     $(`.number`).draggable();
 
+    //Makes the calculator box able to receive dragged numbers.
     $(`#drag-box`).droppable({
         drop: function(event, ui) {
+            //Set the box's text to the result of the current operation.
             $(this).text(currOperation(Number.parseInt($(this).text()), Number.parseInt(ui.draggable.text())));
+            //Remove the dragged number.
             ui.draggable.remove();
             
+            //If there's no more numbers, then trigger the dialog box.
             if($(`.number`).length == 0) {
                 $(`#end-popup`).dialog(`open`);
             }
